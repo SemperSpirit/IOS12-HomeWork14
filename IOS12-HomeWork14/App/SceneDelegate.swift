d66de3ac2f636e3dd3a2a2349e6010ddc5b72659
SemperSpirit <kaiten-aiko@mail.ru>
2024-03-27 21:19:39 +0300

    add code view and images

--- IOS12-HomeWork14/App/SceneDelegate.swift
+++ IOS12-HomeWork14/App/SceneDelegate.swift
@@ -14,8 +14,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-//        let viewController = PhotoAlbums()
-        window?.rootViewController = ViewController()
+        let viewController = PhotoAlbums()
+        window?.rootViewController = PhotoAlbums()
         window?.makeKeyAndVisible()
     }
 }
